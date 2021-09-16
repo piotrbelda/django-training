bb237a648c14dfec725b6e79532ea9998c93bd83
piotrek <piotrek.belda@wp.pl>
2021-09-16 20:54:01 +0200

    init commit

---
+++ devsearch/projects/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse("Here are our products")
+
+def project(request, pk):
+    return HttpResponse("Single project" + " " + str(pk))
