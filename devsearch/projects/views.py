13495eb5829f00cec9c1c6bdaa9f49177066d7dd
piotrek <piotrek.belda@wp.pl>
2021-09-21 21:49:25 +0200

    init model

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': "1",
+        "title": "Ecommerce Website",
+        "descritpion": "Fully functional ecommerce website"
+    },
+    {
+        'id': "2",
+        "title": "Portfolio Website",
+        "descritpion": "This was a project where I built out my portfolio"
+    },
+    {
+        'id': '3',
+        "title": "Social Network",
+        "descritpion": "Awesome open source project I am still working on"
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context = {"page": "projects", "number": 10, "projects": projectsList}
+    return render(request, 'projects/projects.html', context=context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', context={"project": projectObj})
